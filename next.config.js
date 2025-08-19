10323d2be3f59a8bda3815ee0c4aa6ca4a568e64
amanjoshi254 <amanjoshi254@gmail.com>
2025-08-19 17:47:56 +0530

    feat: fix

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
-module.exports = { eslint: { ignoreDuringBuilds: true } };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+};
+
+module.exports = nextConfig;
