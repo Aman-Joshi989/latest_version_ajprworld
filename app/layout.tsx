ade5f8bef434eeb4cacfea526b0cb7755e42de6c
amanjoshi254 <amanjoshi254@gmail.com>
2025-08-26 20:42:08 +0530

    feat: added analatics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -33,6 +35,8 @@ export default function RootLayout({
           <Header />
           <main className="min-h-screen">
             {children}
+            <Analytics />
+            <SpeedInsights />
           </main>
           <Footer />
         </ThemeProvider>
